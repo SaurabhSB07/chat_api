4829241ab43e407bc690b222800a8fe05ca5e617
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-08-11 20:36:47 +0530

    Created registeration serilizer,view and urls

---
+++ aichat_api/chat/srializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from . models import *
+
+class RegisterationSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=128,write_only=True,style={"input_type":"password"})
+    class Meta:
+        model=User
+        fields=["username","password"]
+
+    def create(self, validated_data):
+        password=validated_data.pop("password")
+        user=User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user 
+    
+class UserLoginSerializer(serializers.Serializer):
+    username=serializers.CharField(max_length=50, unique=True)  
+    password=serializers.CharField(max_length=128,write_only=True,style={"input_type":"password"})
+
+    class Meta:
+        model=User
+        fields=["tokens"]  
