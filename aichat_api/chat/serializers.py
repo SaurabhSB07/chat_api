42501256f7dbccc025afb512fbcb14f52f948301
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-08-14 17:07:33 +0530

    updating serializers.py

--- aichat_api/chat/serializers.py
+++ aichat_api/chat/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
-from .models import User,Chat
-from django.contrib.auth.hashers import make_password, check_password
+from .models import User
+from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        user = User.objects.create(**validated_data)
-        return user
+        return User.objects.create_user(**validated_data)
+
 
 
 class LoginSerializer(serializers.Serializer):
@@ -22,36 +22,12 @@ class LoginSerializer(serializers.Serializer):
         username = data.get('username')
         password = data.get('password')
 
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            raise serializers.ValidationError("User not found.")
-
-        if not check_password(password, user.password):
-            raise serializers.ValidationError("Incorrect password.")
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
 
         refresh = RefreshToken.for_user(user)
         return {
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         }
-
-class ChatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chat
-        fields = ['id', 'message', 'response', 'created_at']
-
-    def validate(self, attrs):
-        user = self.context['request'].user
-        if not user.is_authenticated:
-            raise serializers.ValidationError({"msg": "User authentication required"})
-        if user.tokens < 100:
-            raise serializers.ValidationError({"msg": "Not enough tokens"})
-        return attrs
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        user.tokens -= 100
-        user.save()
-        validated_data['user'] = user
-        return super().create(validated_data)
